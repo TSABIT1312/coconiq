5a25908ea009e324e1aea9426ba200a9f3510f8a
TSABIT1312 <sabitalfikri@gmail.com>
2026-07-07 18:48:34 +0700

    contact: add email, address, and location map

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -7,9 +7,10 @@ export const siteConfig = {
   defaultOgImage: '',
   contact: {
     whatsappNumber: '6289664008880',
-    email: '',
-    address: '',
-    mapsEmbedUrl: '',
+    email: 'coconiqindonesia@gmail.com',
+    address: 'Jl. Meranti Gang Meranti VI No. 50 A RT 1 Darat Sekip, Pontianak Kota, Kota Pontianak, Kalbar, 78117 - INDONESIA',
+    mapsEmbedUrl:
+      'https://www.openstreetmap.org/export/embed.html?bbox=109.3291%2C-0.0337%2C109.3411%2C-0.0257&layer=mapnik&marker=-0.0296926%2C109.3350753',
   },
   social: {
     instagram: '',
